fd3bca3cbb3c8e96615d272ad9f5a5210f88a609
Andrej <andrej@pud.ca>
2013-12-23 22:27:12 -0500

    New layout sexiness

---
+++ zdenaskitchen/footer.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package _s
+ */
+?>
+		<footer id="colophon" class="site-footer" role="contentinfo">
+			<div class="site-info">
+				
+			</div><!-- .site-info -->
+		</footer><!-- #colophon -->
+	</div><!-- #content -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
